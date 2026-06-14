06f4a71c4015712ccdb93838ff0749ebe62c43f1
UsamaFar <osamafarooqi331@gmail.com>
2026-06-14 19:47:38 +0500

    initial devops app

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('<h1>Hello DevOps!</h1>');
+});
+app.listen(3000, () => console.log('App running on port 3000'));
